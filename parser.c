d19932b9cd1fb66b4d854c921e7395d8f6a0ea15
ayoub <ayoubaitelaouad@gmail.com>
2023-06-22 15:53:29 +0100

    hot fix

--- parser.c
+++ parser.c
@@ -9,15 +9,15 @@
 instruction_t *parse(int line_number, char *line)
 {
 	instruction_t *result = NULL;
-	char *opcodes[] = {"push", "pall", "pint"};
-	func_t functions[] = {&push, &pall, &pint};
+	char *opcodes[] = {"push", "pall", "pint", "pop"};
+	func_t functions[] = {&push, &pall, &pint, &pop};
 	int i;
 	char *buffer = NULL;
 
 	char *opcode = get_opcode(line);
 	if (!opcode)
 		return (NULL);
-	for (i = 0; i < 3; i++)
+	for (i = 0; i < 4; i++)
 	{
 		if (_strcmp(opcode, opcodes[i]))
 		{
